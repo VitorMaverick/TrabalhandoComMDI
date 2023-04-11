2d0e8c9fb0fac7a944b921ed25db2285cbfb9c56
Vitor Maverick <mr.maverick1@gmail.com>
2023-04-10 21:38:56 -0300

    Update Form1.cs

--- IntroASistemas/Form1.cs
+++ IntroASistemas/Form1.cs
@@ -25,6 +25,32 @@ namespace IntroASistemas
         private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             FrmFilho formFilho = new FrmFilho();
+            formFilho.MdiParent= this;
+            formFilho.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {       
+            string msg, titulo;
+            titulo = "Sair da aplicação";
+            msg = "Você deseja realmente sair mano?";
+               
+         
+            MessageBoxButtons msgButtons = MessageBoxButtons.YesNo;
+
+            DialogResult result =  MessageBox.Show(msg, titulo, msgButtons, MessageBoxIcon.Question);
+
+            if(result == DialogResult.Yes) {
+                Application.Exit();
+            }
+
+            
+        }
+
+        private void toolStripBtnCliente_Click(object sender, EventArgs e)
+        {
+            FrmFilho formFilho = new FrmFilho();
+            formFilho.MdiParent = this;
             formFilho.Show();
         }
     }
